a0bf81e634d2205b712256a5955ff34cca19aeb6
Akhtungg <indeks_dou-jonsa@mail.ru>
2024-08-22 23:25:10 +0300

    ver 0.1
    first version with a connected database, but without tables

---
+++ server/routes/index.js
@@ -0,0 +1,13 @@
+const Router = require('express')
+const router = new Router()
+
+
+router.use('/user')
+router.use('/type')
+router.use('/brand')
+router.use('/device')
+
+
+
+
+module.exports = router
